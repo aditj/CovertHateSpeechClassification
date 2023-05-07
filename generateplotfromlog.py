0a6a921cf57c05031b4a0cac185033ee9093c974
Adit Jain <aditjain1980@gmail.com>
2023-05-07 23:25:22 +0000

    added plot generation files and markov chain

---
+++ generateplotfromlog.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+### Read lines from cmd.log and plot the accuracies
+accuracies = []
+i = 0
+with open("cmd.log") as file:
+    for line in file:
+        i += 1
+        if "Accuracies" in line:
+            ## append the float after the string "Accuracies: " and before " "
+            accuracies.append(float(line.split("Accuracies: ")[1].split(" ")[0]))
+        else:
+            if len(accuracies) > 0:
+                accuracies.append(accuracies[-1])
+            else:
+                accuracies.append(0)
+
+plt.plot(range(i),accuracies)
+plt.xlabel("Communication Round")
+plt.ylabel("Accuracies of the Aggregated Model")
+plt.savefig("./data/accuracies.eps")
